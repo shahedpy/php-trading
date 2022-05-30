e9cfce486d09ddca7cb322afe3ade61ed0fd4133
Shahed <shahed_hridoy@outlook.com>
2022-05-30 13:19:47 +0600

    update

--- login/db.php
+++ login/db.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = mysqli_connect("localhost", "root", "12345678", "project");
+    $conn = mysqli_connect("localhost", "root", "", "project");
 
     if(!$conn){
         echo "<h3 class='container my-3 text-center bg-dark text-white rounded-lg p-3'>Unable to establish connection to Database</h3>";
