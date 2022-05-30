d9855cd14754718d9ab3d43099adca5229c80b7a
Shahed <shahed_hridoy@outlook.com>
2022-05-31 02:13:17 +0600

    registration api updated

--- api/registration.php
+++ api/registration.php
@@ -3,52 +3,35 @@ include 'include.php';
 
 
 
+if($_SERVER['REQUEST_METHOD'] == 'POST' ){
+
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $nid = $_POST['nid'];
+    $refferal = $_POST['refferal'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    if($password != $confirm_password){
+        echo 'Password and confirm password are not same';
+    }
+
+    $enc_pass = md5($password);
 
 
-// include 'include.php';
+    $sql = "INSERT INTO `users` (`id`, `name`, `phone`, `nid`, `referral_id`, `password`) 
+                        VALUES (NULL, '$name', '$phone', '$nid', '$refferal', '$enc_pass')";
 
-// if($_SERVER['REQUEST_METHOD'] == 'POST' ){
+if ($conn->query($sql) === TRUE) {
+  echo 'Data inserted';
 
-//     $name = $_POST['name'];
-//     $phone = $_POST['phone'];
-//     $nid = $_POST['nid'];
-//     $refferal = $_POST['refferal'];
-//     $password = $_POST['password'];
-//     $confirm_password = $_POST['confirm_password'];
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-//     if($password != $confirm_password){
-//         echo 'Password and confirm password are not same';
-//     }
-
-//     $enc_pass = md5($password);
+$conn->close();
 
 
-//     $sql = "INSERT INTO `users` (`id`, `name`, `phone`, `nid`, `referral_id`, `password`) 
-//                         VALUES (NULL, '$name', '$phone', '$nid', '$refferal', '$enc_pass')";
-
-    
-
-// if ($conn->query($sql) === TRUE) {
-
-
-
-//   //read data
-
-
-//   //create data
-
-
-
-
-
-
-//   } else {
-//     echo "Error: " . $sql . "<br>" . $conn->error;
-//   }
-  
-//   $conn->close();
-
-
-// } else {
-//     header('location: ../login/registration.php');
-// }
+} else {
+  header('location: ../login/registration.php');
+}
