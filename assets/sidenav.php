fd930242dfad302e8339bc398ed8d00ac5bef863
Shahed <shahed_hridoy@outlook.com>
2022-05-31 01:48:19 +0600

    sidenav updated

--- assets/sidenav.php
+++ assets/sidenav.php
@@ -35,6 +35,12 @@ session_start();
                         <p>Dashboard</p>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer"></i>
+                        <p>Refferal User</p>
+                    </a>
+                </li>
 
             </ul>
         </nav>
