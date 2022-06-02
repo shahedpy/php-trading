1a9428e7ae87db39f6a9163d87fa03b5d3fbb22e
Shahed <shahed_hridoy@outlook.com>
2022-06-03 03:52:32 +0600

    database updated

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 01, 2022 at 10:40 PM
+-- Generation Time: Jun 02, 2022 at 11:51 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -59,6 +59,31 @@ CREATE TABLE `leaderboard` (
   `hitted_date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `leaderboard`
+--
+
+INSERT INTO `leaderboard` (`id`, `voucher`, `hitted_by`, `hitted_date`) VALUES
+(42, 2, '01821090909', '2022-06-03 03:36:30'),
+(43, 2, '01821090909', '2022-06-03 03:42:49'),
+(44, 2, '01821090909', '2022-06-03 03:42:49'),
+(45, 2, '01821090909', '2022-06-03 03:43:10'),
+(46, 2, '01821090909', '2022-06-03 03:43:10'),
+(47, 2, '01821090909', '2022-06-03 03:43:21'),
+(48, 2, '01821090909', '2022-06-03 03:43:21'),
+(49, 2, '01821090909', '2022-06-03 03:44:00'),
+(50, 2, '01821090909', '2022-06-03 03:44:00'),
+(51, 2, '01821090909', '2022-06-03 03:45:12'),
+(52, 2, '01821090909', '2022-06-03 03:45:12'),
+(53, 2, '01821090909', '2022-06-03 03:47:22'),
+(54, 2, '01821090909', '2022-06-03 03:47:22'),
+(55, 2, '01821090909', '2022-06-03 03:47:57'),
+(56, 2, '01821090909', '2022-06-03 03:47:57'),
+(57, 2, '01821090909', '2022-06-03 03:48:46'),
+(58, 2, '01821090909', '2022-06-03 03:48:46'),
+(59, 2, '01821090909', '2022-06-03 03:49:11'),
+(60, 2, '01821090909', '2022-06-03 03:49:11');
+
 -- --------------------------------------------------------
 
 --
@@ -70,7 +95,7 @@ CREATE TABLE `users` (
   `name` varchar(200) NOT NULL,
   `phone` varchar(11) NOT NULL,
   `nid` varchar(17) NOT NULL,
-  `referral_id` varchar(11) NOT NULL,
+  `parent` varchar(11) NOT NULL,
   `password` varchar(200) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -79,9 +104,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `phone`, `nid`, `referral_id`, `password`, `created_at`) VALUES
+INSERT INTO `users` (`id`, `name`, `phone`, `nid`, `parent`, `password`, `created_at`) VALUES
 (1, 'Rafeu Ahammed', '01821090909', '', '0', '21232f297a57a5a743894a0e4a801fc3', '2022-05-31 02:50:06'),
-(4, 'Rafeu Ahammed', '01742057743', '1', '123', 'c20ad4d76fe97759aa27a0c99bff6710', '2022-05-31 02:50:06'),
 (5, 'Abir', '01848125325', '23426534573248563', '1821090909', '21232f297a57a5a743894a0e4a801fc3', '2022-05-31 02:50:06'),
 (6, 'Akash', '01912121212', '84304840483940581', '01821090909', '21232f297a57a5a743894a0e4a801fc3', '2022-05-31 02:50:06'),
 (7, 'a', '9', '1111', '01821090909', '21232f297a57a5a743894a0e4a801fc3', '2022-05-31 02:50:06'),
@@ -90,7 +114,10 @@ INSERT INTO `users` (`id`, `name`, `phone`, `nid`, `referral_id`, `password`, `c
 (10, 'korim', '01742057722', '012345684244446', '01742057743', '21232f297a57a5a743894a0e4a801fc3', '2022-05-31 02:50:42'),
 (11, 'rohim', '01742057732', '5454154455895225', '01742057743', '21232f297a57a5a743894a0e4a801fc3', '2022-05-31 03:00:55'),
 (13, 'rohim', '01742057733', '5454154455895224', '01742057743', '21232f297a57a5a743894a0e4a801fc3', '2022-05-31 03:01:41'),
-(14, 'Batash', '01821112211', '19927384783984739', '01821090909', '202cb962ac59075b964b07152d234b70', '2022-06-02 02:01:59');
+(14, 'Batash', '01821112211', '19927384783984739', '01821090909', '202cb962ac59075b964b07152d234b70', '2022-06-02 02:01:59'),
+(15, 'Andaman', '01621090909', '12285484995884858', '01821090909', '21232f297a57a5a743894a0e4a801fc3', '2022-06-03 02:48:47'),
+(16, 'BBBB', '01921009900', '949304098589', '01821090909', '21232f297a57a5a743894a0e4a801fc3', '2022-06-03 02:51:28'),
+(17, 'AAAAAAAAA', '01221122112', '43333333333333333', '01821090909', '21232f297a57a5a743894a0e4a801fc3', '2022-06-03 02:53:43');
 
 -- --------------------------------------------------------
 
@@ -100,12 +127,43 @@ INSERT INTO `users` (`id`, `name`, `phone`, `nid`, `referral_id`, `password`, `c
 
 CREATE TABLE `voucher` (
   `id` int(11) NOT NULL,
-  `voucher_no` int(11) NOT NULL,
   `voucher_limit` int(11) NOT NULL,
   `owned_by` varchar(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `voucher`
+--
+
+INSERT INTO `voucher` (`id`, `voucher_limit`, `owned_by`, `created_at`, `updated_at`) VALUES
+(1, 0, '01821090909', '2022-06-03 01:10:43', '2022-06-03 02:39:36'),
+(2, 4, '01821090909', '2022-06-03 01:28:08', '2022-06-03 03:49:11'),
+(3, 0, '01742057743', '2022-06-03 01:28:36', '2022-06-03 02:28:50'),
+(4, 0, '01821090909', '2022-06-03 01:47:11', '2022-06-03 02:24:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet`
+--
+
+CREATE TABLE `wallet` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(11) NOT NULL,
+  `amount` float NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `wallet`
+--
+
+INSERT INTO `wallet` (`id`, `phone`, `amount`, `created_at`, `updated_at`) VALUES
+(1, '01821090909', 10, '2022-06-03 02:43:35', '2022-06-03 03:03:22'),
+(2, '01221122112', 50, '2022-06-03 02:53:43', '2022-06-03 03:07:10');
 
 --
 -- Indexes for dumped tables
@@ -138,6 +196,13 @@ ALTER TABLE `voucher`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `wallet`
+--
+ALTER TABLE `wallet`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `phone` (`phone`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -151,19 +216,25 @@ ALTER TABLE `data`
 -- AUTO_INCREMENT for table `leaderboard`
 --
 ALTER TABLE `leaderboard`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `voucher`
 --
 ALTER TABLE `voucher`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `wallet`
+--
+ALTER TABLE `wallet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
